906f439aa0c34e0c9b9c0741d50a11e4aa2aaee5
Mark Josmil Degamo <MDegamo01@lear.com>
2022-08-22 10:30:27 +0800

    Add project files.

---
+++ EmployeeSalaryCalculationSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeSalaryCalculationSystem.MvcApplication" Language="C#" %>
---
+++ EmployeeSalaryCalculationSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeeSalaryCalculationSystem.Startup))]
+namespace EmployeeSalaryCalculationSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
